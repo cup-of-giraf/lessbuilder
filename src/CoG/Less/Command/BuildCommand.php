c209198848e1684c26fe8ddc32616171a8f428fb
Pierre Tachoire <ptachoire@itnetwork.fr>
2012-11-22 22:32:56 +0100

    help translation

--- src/CoG/Less/Command/BuildCommand.php
+++ src/CoG/Less/Command/BuildCommand.php
@@ -12,24 +12,26 @@ class BuildCommand extends Command
 {
     protected $less;
 
+    protected $output;
+
     protected function configure()
     {
         $this
             ->setName('build')
-            ->setDescription('Permet de compiler un fichier less au format css')
-            ->addArgument('source', InputArgument::REQUIRED, 'Fichier ou dossier less')
-            ->addOption('target', 't', InputOption::VALUE_OPTIONAL, 'Dossier de sortie');
+            ->setDescription('Build less files into css')
+            ->addArgument('source', InputArgument::REQUIRED, 'File or directory to build')
+            ->addOption('target', 't', InputOption::VALUE_OPTIONAL, 'Output file or directory');
         $this->less = new \lessc();
     }
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
+        $this->output = $output;
         $source = new \SplFileObject($input->getArgument('source'));
-
         $target = $input->getOption('target') != null ? new \SplFileInfo($input->getOption('target')) : null;
 
         if( $source->isDir() ) {
-            throw new \InvalidArgumentException('On ne gère pas encore les repertoires');
+            throw new \InvalidArgumentException('Directories are not yet supported');
         }
 
         $this->compile( new \SplFileObject($source), $target);
@@ -39,7 +41,15 @@ class BuildCommand extends Command
     protected function compile( \SplFileObject $source, \SplFileInfo $target=null ) {
 
         $real_target = $this->getRealTarget($source, $target);
+        $this->output->write(
+            sprintf(
+                'Building <info>%s</info> into <info>%s</info> ',
+                $source->getPathname(),
+                $real_target->getPathname()
+            )
+        );
         $this->less->compileFile( $source->getPathname(), $real_target->getPathname() );
+        $this->output->writeln(sprintf('<%s>[%s]</%s>', 'info', ' OK ', 'info' ));
     }
 
     /**
